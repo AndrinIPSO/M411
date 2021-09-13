cc1310e59711d32668d83a828672000ef068ccff
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-09-13 11:12:56 +0200

    Nodes

---
+++ LinkedList/LinkedListSimple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListSimple
+    {
+        private sealed class Node
+        {
+            public object Item { get; set; }
+            public Node Next { get; set; }
+
+            public Node()
+            {
+
+            }
+
+            public Node(object it)
+            {
+                Item = it;
+            }
+        }
+
+        private Node first = null;
+        private Node last = null;
+
+        public void Add(object item)
+        {
+            Node n = new Node() {Item = item, Next = null};
+            if (first == null)
+            {
+                first = n;
+                last = n;
+            }
+            else
+            {
+                last.Next = n;
+                last = n;
+                
+                // oder ohne last
+                //Node curr = first;
+                //while (curr.Next != null)
+                //{
+                //    curr = curr.Next;
+                //}
+                //curr.Next = n;
+            }
+        }
+    }
+}
