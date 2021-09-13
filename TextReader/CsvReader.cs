bd0474f3151841cfddeab37844de43269299d0f2
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-09-13 08:55:31 +0200

    xc

---
+++ TextReader/CsvReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextReader
+{
+    class CsvReader
+    {
+        public string Load(string filename)
+        {
+            CsvReader
+        }
+    }
+}
